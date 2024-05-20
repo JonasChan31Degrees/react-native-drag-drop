e92fd764dd6f92410eddb011e4f435c7ff0332eb
Jonas Chan <jonas.chan@31degrees.co.uk>
2024-05-20 22:38:48 +0100

    Set delayLongPress to 150

--- src/lib/Draggable.tsx
+++ src/lib/Draggable.tsx
@@ -143,7 +143,7 @@ class Draggable extends Component<DraggableProps, DraggableState> {
         style={[panStyle, style]}
       >
         <TouchableOpacity
-          delayLongPress={100}
+          delayLongPress={150}
           onLongPress={() => this.setState({ pressed: true }, () => {})}
           onPress={this.onPress}
           {...this.props.propsInItems}
